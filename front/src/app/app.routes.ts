e57673e6b867119f09b2ad217983476eac37f2c5
NexWan <lcontrerasmartinez235@gmail.com>
2024-04-20 02:45:07 -0600

    Added 404 to front end

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent,},
     {path: 'home', component: HomeComponent},
+    {path: '**', component: NotFoundComponent}
 ];
 
 /*Static routes of the front*/
